5dbd7439519726288154d7c470f7e8bca13b6e21
CWood-sdf <woodchristophermichael@gmail.com>
2023-02-28 20:57:07 -0500

    ook

--- build/include/include/Sensors/PotDial.h
+++ build/include/include/Sensors/PotDial.h
@@ -2,7 +2,6 @@
 #define POTDIAL_H
 
 #include "Odometry/EPA_Tracker.h"
-typedef triport::port port;
 class PotDial;
 class SelectorArr {
     typedef function<void()> FnTp;
@@ -10,14 +9,46 @@ class SelectorArr {
     static inline vector<PotDial*> boundDials = {};
     static inline map<vector<int>, FnTp> toVal = {};
     vector<int> count;
+    FnTp fn = emptyFn;
     friend class PotDial;
     static void addDial(PotDial* dial);
 
 public:
+    /**
+     * @brief Construct a new Selector Arr object
+     *
+     * @param count The desired value of the dials
+     * @param fn The function to be called when the dials are set to the desired value
+     */
     SelectorArr(vector<int> count, FnTp fn);
+    /**
+     * @brief Construct a new Selector Arr object
+     *
+     * @param count The desired value of the dials
+     */
     SelectorArr(vector<int> count);
+    /**
+     * @brief Construct a new Selector Arr object
+     *
+     */
     SelectorArr() {}
+    /**
+     * @brief Set the function to be called when the dials are set to the desired value
+     *
+     * @param fn
+     */
     void attachFn(FnTp fn);
+    /**
+     * @brief Set the desired value of the dials
+     *
+     * @param count The desired value of the dials
+     */
+    void setArray(vector<int> count);
+    /**
+     * @brief Get the function to be called given the dials' current values
+     *
+     * @return FnTp
+     */
     static FnTp getVal();
 };
 class PotDial {
@@ -39,16 +70,38 @@ class PotDial {
     }
 
 public:
-public:
+    /**
+     * @brief Construct a new Pot Dial object
+     *
+     * @tparam Args
+     * @param sensor The potentiometer to be used
+     * @param otherArgs The private constructor arguments
+     */
     template <typename... Args>
     PotDial(pot& sensor, Args... otherArgs) : PotDial(otherArgs...) {
         this->sensor = &sensor;
     }
+    /**
+     * @brief Construct a new Pot Dial object
+     *
+     * @tparam Args
+     * @param p The port to be used
+     * @param args The private constructor arguments
+     */
     template <typename... Args>
-    PotDial(port& p, Args... args) : PotDial(args...) {
+    PotDial(triport::port& p, Args... args) : PotDial(args...) {
         sensor = new pot(p);
     }
+    /**
+     * @brief Return the value from the potentiometer
+     *
+     * @return int
+     */
     int getAmnt();
+
+    int raw() {
+        return sensor->angle(degrees);
+    }
 };
 
 #endif // POTDIAL_H
